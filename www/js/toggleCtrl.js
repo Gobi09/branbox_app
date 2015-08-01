144fb1c44adf3025f84f86fe46ed931ce063d5fe
Gobi <gobi@appnlogic.com>
2015-08-01 16:43:51 +0530

    origins

--- www/js/toggleCtrl.js
+++ www/js/toggleCtrl.js
@@ -84,8 +84,8 @@ angular.module('starter.toggleCtrl', [
         }
          tx.executeSql('SELECT * FROM menu where status="ON" and businessId = "'+businessId+'" ',[], function (tx, results) {
           var itemLength = results.rows.length;
-          $scope.Menus=results.rows;
-          alert(results);
+          $scope.Menus=results.rows.item;
+          alert(results.rows.item);
           alert(results.rows.item(0).businessId);
         });
     
